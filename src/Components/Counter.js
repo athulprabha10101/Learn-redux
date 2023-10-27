52975f5afab1f366c6d0be4e0fe8a2449296f97f
athulprabha10101 <alertsforathul@gmail.com>
2023-10-27 17:07:20 +0530

    initial commit

---
+++ src/Components/Counter.js
@@ -0,0 +1,12 @@
+function Counter() {
+  return (
+    <div className="counter">
+      <h2>Counter</h2>
+      <h3>The count is - {}</h3>
+      <button>Increase</button>
+      <button>Decrease</button>
+    </div>
+  );
+}
+
+export default Counter;
