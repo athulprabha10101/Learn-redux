35868342ca39a0fe58f233c0f0622ece928118e9
athulprabha10101 <alertsforathul@gmail.com>
2023-10-28 15:34:51 +0530

    Final commit

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,10 +1,24 @@
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { increase_count } from "../Redux/count/countSlice";
+import { decrease_count } from "../Redux/count/countSlice";
+
 function Counter() {
+  const count = useSelector(state=> state.count.value)
+  const dispatch = useDispatch()
+
+  const increment =()=>{
+    dispatch(increase_count())
+  }
+  const decrement = ()=>{
+    dispatch(decrease_count())
+  }
+
   return (
     <div className="counter">
       <h2>Counter</h2>
-      <h3>The count is - 0</h3>
-      <button>Increase</button>
-      <button>Decrease</button>
+      <h3>The count is - {count}</h3>
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
     </div>
   );
 }
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import colorReducer from "./color/colorSlice"
+import countReducer from "./count/countSlice"
 
 export default configureStore({
-    reducer:{color:colorReducer}
+    reducer:{
+        color:colorReducer,
+        count:countReducer
+    }
+
 })
